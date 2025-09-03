151c2e7a8ccbc13956bd3b10cd5c1d42fe9ecf68
RavenSimone19 <ravensimonedarapiza19@gmail.com>
2025-09-03 20:13:28 +0800

    Second Commit

--- Multiplication_Table.py
+++ Multiplication_Table.py
@@ -1,15 +1,25 @@
-size = 10
+def search_matrix():
+    while True:
+        try:
+            row = int(input("Enter row: "))
+            col = int(input("Enter col: "))
+            search_val = int(input("Search: "))
+        except ValueError:
+            print("Invalid input. Please enter integers only.")
+            continue
 
-print("    ", end="")
-for i in range(1, size + 1):
-    print(f"{i:4}", end="")
-print()
+        if row <= 0 or col <= 0:
+            print("Stop the loop")
+            break
 
-print("    " + "----" * size)
+        matrix = [[i * j for j in range(1, col + 1)] for i in range(1, row + 1)]
 
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == search_val:
+                    print(f"[{matrix[i][j]}]", end=" ")
+                else:
+                    print(matrix[i][j], end=" ")
+            print()
 
-for i in range(1, size + 1):
-    print(f"{i:3} |", end="")  
-    for j in range(1, size + 1):
-        print(f"{i * j:4}", end="")
-    print()
+search_matrix()
